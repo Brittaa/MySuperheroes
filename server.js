f341946e94d68a023547567a92f04cb67c42284d
Your Name <britta.panker@gmail.com>
2020-09-09 10:52:19 +0300

    first

--- server.js
+++ server.js
@@ -6,6 +6,25 @@ superheroes.all;
 let randomHero = superheroes.random();
 console.log(randomHero);
 
-superheroes.all.forEach(hero =>{
+/*superheroes.all.forEach(hero =>{
     console.log(hero);
-});
+});/*
+/*
+TASK 1
+1. get the names of all supervillains
+2. get a random supervillains name
+3.* make a random superhero flight a random supervillain
+in the console.log()
+*/
+
+let supervillains = require('supervillains');
+supervillains.all;
+
+let randomVillain = supervillains.random();
+console.log(randomVillain);
+
+/*supervillains.all.forEach(villain =>{
+    console.log(villain);
+});*/
+
+console.log(`Superhero ${randomHero} is fighting with supervillain ${randomVillain}.`);
